b3a00280d9ec846d5fefb6c4f02cbc10c328c853
= <=>
2018-08-14 09:19:17 -0500

    basic DFT

--- Effects.py
+++ Effects.py
@@ -2,5 +2,12 @@ import scipy.io.wavfile
 import matplotlib.pyplot as plt
 import numpy as np
 
-rate, data = scipy.io.wavfile.read('C:\Users\s524063\Desktop\AudioTestPython\Guit_44.1k_24b_None.wav')
+rate, data = scipy.io.wavfile.read('files\Guit_44.1k_32b_None.wav')
 
+t = (np.fft.fft(data))
+
+X = np.linspace(0, len(t), len(t))
+
+plt.plot(X, t)
+plt.plot(X, data)
+plt.show()
